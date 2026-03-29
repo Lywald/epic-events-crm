79b932202fbc5daddd3d8a15ecc64c7582619140
Igor <pierre.zarebski@live.fr>
2026-03-29 18:49:49 +0200

    class Database

---
+++ database/database.py
@@ -0,0 +1,10 @@
+class Database:
+    def __init__(self):
+        return
+    
+    def LoginDatabase(username: str, password: str):
+        return
+    
+    def RequestDatabase(request: str):
+        """Protéger les requètes contre injection SQL"""
+        return
